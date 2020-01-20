d849de90f7ad409eacae54135b6fd0522db5c855
Rachelle <rachellerathbone@gmail.com>
2020-01-19 20:10:26 -0800

    wip: add login functionality

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -10,3 +10,31 @@ import org.springframework.security.config.annotation.web
 import org.springframework.security.config.annotation.web
                         .configuration.WebSecurityConfigurerAdapter;
 //end::baseBonesImports[]
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Overide
+	protected void configure(AuthenticationManagerBuilder auth)
+		throws Exception {
+		
+	}
+	
+	@Bean
+	  public PasswordEncoder encoder() {
+	    return new StandardPasswordEncoder("53cr3t");
+	  }
+	
+	@Override
+	  protected void configure(AuthenticationManagerBuilder auth)
+	      throws Exception {
+		
+		// make a call to the userDetailsService() method, passing in the 
+		// userDetailsService instance that has been autowired into SecurityConfig
+	    auth
+	      .userDetailsService(userDetailsService)
+	      .passwordEncoder(encoder());
+
+	  }
+}
